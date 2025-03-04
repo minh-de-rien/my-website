5b11016a27d08a26e2996b19d0ae2b23b6b69562
Minh Ngoc Do <minhngocdo.mnd@gmail.com>
2025-03-04 16:48:53 -0500

    * Add GitHub actions for linting * Add somemore prettier rules

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,6 +1,13 @@
 /** @type {import("prettier").Config} */
 export default {
     tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    trailingComma: "all",
+    bracketSpacing: true,
+    arrowParens: "always",
+    printWidth: 100,
     plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
     overrides: [
         {
