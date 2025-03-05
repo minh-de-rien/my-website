1f093db5112948394e2843b55f371d01a68c6c5a
Minh Ngoc Do <minhngocdo.mnd@gmail.com>
2025-03-05 16:00:53 -0500

    Add a navbar * Add ToggleSwitch for light and dark mode * Add navbar, locales and icons * Update tsconfig with new paths, astro.config with i18n package * Update all files with prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,33 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import i18n from '@astrolicious/i18n';
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
+
+const defaultLocale = 'fr';
+const locales = ['fr', 'en'];
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://example.com",
-    integrations: [mdx(), sitemap()],
+    site: 'https://example.com',
+    integrations: [
+        mdx(),
+        i18n({
+            defaultLocale: defaultLocale,
+            locales: locales,
+        }),
+        sitemap({
+            i18n: {
+                defaultLocale: defaultLocale,
+                locales: {
+                    fr: 'fr',
+                },
+            },
+        }),
+        icon(),
+    ],
     vite: {
         plugins: [tailwindcss()],
     },
